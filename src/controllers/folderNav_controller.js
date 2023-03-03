7c5c4429a00e94af0c0a725aefae72e8594e0c9b
Andy Smith <asmith.esq@gmail.com>
2023-03-03 15:07:16 -0600

    Hook up Stimulus

---
+++ src/controllers/folderNav_controller.js
@@ -0,0 +1,39 @@
+import { Controller } from "@hotwired/stimulus";
+
+const SELECTED_CLASS = "folder-nav__item--selected";
+
+export default class extends Controller {
+  static targets = ["list"];
+  static values = {
+    index: {
+      type: Number,
+      default: -1,
+    },
+  };
+
+  select(event) {
+    const button = event.target;
+    const item = button.closest(".folder-nav__item");
+    const list = button.closest(".folder-nav__list");
+    const items = list.children;
+
+    this.indexValue = this.listTargets.indexOf(list);
+    this.showLists();
+
+    for (let item of items) {
+      item.classList.remove(SELECTED_CLASS);
+    }
+    item.classList.add(SELECTED_CLASS);
+  }
+
+  indexValueChanged() {
+    console.log("indexValue Changed");
+    this.showLists();
+  }
+
+  showLists() {
+    this.listTargets.forEach((element, index) => {
+      element.hidden = index > this.indexValue + 1;
+    });
+  }
+}
